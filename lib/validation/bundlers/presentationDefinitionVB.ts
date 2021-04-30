7f80b4772e82cdde1c6dc75f585c5c7263c078ef
hrehman-sphereon <hrehman@sphereon.com>
2021-04-30 14:12:28 +0200

    ESSIFI-6 add inputDescriptor validation

--- lib/validation/bundlers/presentationDefinitionVB.ts
+++ lib/validation/bundlers/presentationDefinitionVB.ts
@@ -18,6 +18,14 @@ export class PresentationDefinitionVB extends ValidationBundler<PresentationDefi
 
   private myValidations(pd: PresentationDefinition): Validation<unknown>[] {
     return [
+      // V Section 4.A.A : pd.id must be non-empty.
+      // V Section 4.A.A : MUST be a unique identifier
+      // E Section 4.B   : The Input Descriptors (#term:input-descriptors) required for submission are described by the submission_requirements. If no submission_requirements value is present, all inputs listed in the input_descriptors array are required for submission.
+      // V Section 4.C   : name == null || non-null string.
+      // V Section 4.D   : purpose == null || non-empty string.
+      // V Section 4.E.A : format == null || format.(jwt|jwt_vc|jwt_vp.alg[])|(ldp|ldp_vc|ldp_vp.proof_type[]) should be non-empty strings
+      // V inDesc[i].group[i] : group == null || MUST match one of the grouping strings listed in the from values of a Submission Requirement Rule.
+
       {
         tag: this.getTag(),
         target: pd,
@@ -29,6 +37,10 @@ export class PresentationDefinitionVB extends ValidationBundler<PresentationDefi
 
   private static shouldBeNonEmptyArray(pd: PresentationDefinition): boolean {
     // TODO extract to generic utils or use something like lodash
-    return pd.input_descriptors != null && pd.input_descriptors.length > 0;
+    return (
+      pd.input_descriptors != null &&
+      Array.isArray(pd.input_descriptors) &&
+      pd.input_descriptors.length > 0
+    );
   }
 }
