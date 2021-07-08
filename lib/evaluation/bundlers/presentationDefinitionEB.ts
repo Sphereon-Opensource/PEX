590de9305890ec813ca8ae171456568b3812b7b8
sksadjad <86593551+sksadjad@users.noreply.github.com>
2021-07-08 15:52:37 +0200

    ESSIFI-17: added a success test, plus removing some unnecessary code

--- lib/evaluation/bundlers/presentationDefinitionEB.ts
+++ lib/evaluation/bundlers/presentationDefinitionEB.ts
@@ -26,21 +26,34 @@ export class PresentationDefinitionEB extends EvaluationBundler<any[]> {
       {
         tag: this.getTag(),
         target: params,
-        predicate: PresentationDefinitionEB.getPredicate,
+        predicate: PresentationDefinitionEB.evaluateUri,
         message: 'presentation_definition URI for the schema of the candidate input MUST be equal to one of the input_descriptors object uri values exactly.',
       },
     ];
   }
 
-  private static getPredicate(params: any[]) : boolean {
+  private static evaluateUri(params: any[]) : boolean {
     let uris: string[] = params[1].input_descriptors.map(id => id.schema.map(so => so.uri)).flat();
-    // console.log(uris)
-    return PresentationDefinitionEB.stringIsPresentInList(params[0].definition_id, uris);
+    for (let i = 0; i < params[0].verifiableCredential.length; i++) {
+      const vc = params[0].verifiableCredential[i];
+      if (vc.vc) {
+        if (!PresentationDefinitionEB.stringIsPresentInList(vc.vc['@context'], uris)) {
+          return false;
+        }
+      }
+      else if(vc['@context']) {
+        if (!PresentationDefinitionEB.stringIsPresentInList(vc['@context'], uris)) {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+    return true;
   }
 
   private static stringIsPresentInList(presentation_definition_uri: string, input_descriptors_uri: string[]): boolean {
     // TODO extract to generic utils or use something like lodash
-    console.log('>>>>> presentation_definition_uri:', presentation_definition_uri, 'input_descriptors_uri:',input_descriptors_uri)
     return presentation_definition_uri != null && input_descriptors_uri.find(el => el === presentation_definition_uri) != undefined;
   }
 
--- test/evaluation/bundlers/presentationDefinitionEB.spec.ts
+++ test/evaluation/bundlers/presentationDefinitionEB.spec.ts
@@ -12,73 +12,23 @@ function getFile(path: string) {
   return JSON.parse(fs.readFileSync(path, 'utf-8'));
 }
 
-function getTestablePresentationSubmissions(): PresentationSubmission[] {
-  return [
-    {
-      "id": "a30e3b91-fb77-4d22-95fa-871689c322e2",
-      "definition_id": "32f54163-7166-48f1-93d8-ff217bdb0653",
-      "descriptor_map": [
-        {
-          "id": "banking_input_2",
-          "format": "jwt_vc",
-          "path": "$.verifiableCredential[0]"
-        },
-        {
-          "id": "employment_input",
-          "format": "ldp_vc",
-          "path": "$.verifiableCredential[1]"
-        },
-        {
-          "id": "citizenship_input_1",
-          "format": "ldp_vc",
-          "path": "$.verifiableCredential[2]"
-        }
-      ]
-    },
-    {
-      "id": "a30e3b91-fb77-4d22-95fa-871689c322e2",
-      "definition_id": "32f54163-7166-48f1-93d8-ff217bdb0653",
-      "descriptor_map": [
-        {
-          "id": "banking_input_2",
-          "format": "jwt",
-          "path": "$._claim_sources.banking_input_2.JWT"
-        },
-        {
-          "id": "employment_input",
-          "format": "jwt_vc",
-          "path": "$._claim_sources.employment_input.VC_JWT"
-        },
-        {
-          "id": "citizenship_input_1",
-          "format": "ldp_vc",
-          "path": "$._claim_sources.citizenship_input_1.VC"
-        }
-      ]
-    },
-  ];
-}
-
 describe('validate', () => {
 
-  /*test.each(files)(
-    '.validateKnownExample(%s)',
-    (file) => {
-      const basicPD = getFile(base + file);
-
-      const vb: ValidationBundler<PresentationDefinition> = new PresentationDefinitionEB('root');
-
-      const result = new ValidationEngine().validate([{bundler: vb, target: basicPD.presentation_definition}]);
-      expect(result).toEqual([new Checked('root', Status.INFO, 'ok')]);
-    }
-  );*/
-
-  it('should return error for missing id   should return error for not matching (exactly) any URI for the schema of the candidate input with one of the Input Descriptor schema object uri values.', () => {
+  it('should return error for not matching (exactly) any URI for the schema of the candidate input with one of the Input Descriptor schema object uri values.', () => {
+    const vpGenral: PresentationDefinition = getFile('./test/dif_pe_examples/vp/vp_general.json');
     const basicPD: PresentationDefinition = getFile('./test/resources/pd_basic.json');
+
     const eb: EvaluationBundler<PresentationDefinition> = new PresentationDefinitionEB('root');
 
-    const result = new EvaluationEngine().evaluate([{bundler: eb, target: [getTestablePresentationSubmissions(), basicPD]}]);
+    const result = new EvaluationEngine().evaluate([{bundler: eb, target: [vpGenral, basicPD]}]);
     expect(result).toEqual([new Checked('root.presentation_definition', Status.ERROR, 'presentation_definition URI for the schema of the candidate input MUST be equal to one of the input_descriptors object uri values exactly.')]);
   });
 
+  it('should return ok for matching (exactly) one URI for the schema of the candidate input with one of the Input Descriptor schema object uri values.', () => {
+    const vpGeneral: PresentationSubmission = getFile('./test/dif_pe_examples/vp/vp_business.json');
+    const basicPD: PresentationDefinition = getFile('./test/resources/pd_basic.json');
+    const eb: EvaluationBundler<PresentationDefinition> = new PresentationDefinitionEB('root');
+    const result = new EvaluationEngine().evaluate([{bundler: eb, target: [vpGeneral, basicPD]}]);
+    expect(result).toEqual([new Checked('root', Status.INFO, 'ok')]);
+  });
 });
