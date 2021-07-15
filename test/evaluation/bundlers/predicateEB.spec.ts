21dbc0688be45d6825275b94c7f9f02e4fe7c6b2
sksadjad <86593551+sksadjad@users.noreply.github.com>
2021-07-15 10:21:38 +0200

    ESSIFI-104: fixed problems eslint and test problems

--- test/evaluation/bundlers/predicateEB.spec.ts
+++ test/evaluation/bundlers/predicateEB.spec.ts
@@ -12,13 +12,12 @@ function getFile(path: string) {
 
 describe('evaluate', () => {
 
-
   it('should report error if the predicate is present and filter is not', function () {
-    const pdSchema: PresentationDefinition = getFile('./test/dif_pe_examples/pd/pd-simple-schema-age-predicate.json');
+    const pdSchema: PresentationDefinition = getFile('./test/dif_pe_examples/pd/pd-simple-schema-age-predicate.json').presentation_definition;
     delete pdSchema.input_descriptors[0].constraints.fields[0].filter;
 
     const eb: EvaluationBundler<InputDescriptor[], unknown> = new PredicateEB('root');
-    const ee = new EvaluationEngine();
+    const  ee = new EvaluationEngine();
     const result = ee.evaluate([{bundler: eb, target: {d: pdSchema.input_descriptors, p: null}}])
     expect(result[0]).toEqual(new Checked('root.input_descriptor', Status.ERROR, 'if in the field we have predicate value, the filter value should be present as well.'));
     expect(result[1]).toEqual(new Checked('root.input_descriptor', Status.ERROR, 'verifiableCredential\'s matching predicate property should be boolean'));
@@ -26,7 +25,7 @@ describe('evaluate', () => {
 
   //TODO: Talk to Hafeez It's handled by class casting
   it('should return error if value of predicate is not one of [required, preferred]', function () {
-    const pdSchema = getFile('./test/dif_pe_examples/pd/pd-simple-schema-age-predicate.json');
+    const pdSchema = getFile('./test/dif_pe_examples/pd/pd-simple-schema-age-predicate.json').presentation_definition;
     pdSchema.input_descriptors[0].constraints.fields[0].predicate = "necessary";
     const validPredicates = ['required', 'preferred'];
     const exceptionResults = [];
@@ -37,15 +36,14 @@ describe('evaluate', () => {
             exceptionResults.push(new Checked('root.input_descriptor', Status.ERROR, 'predicate value should be one of these values: [\'required\', \'preferred\']'));
           }
         })
-      }
-    })
+      }})
     expect(exceptionResults.length).toEqual(1);
     expect(exceptionResults[0]).toEqual(new Checked('root.input_descriptor', Status.ERROR, 'predicate value should be one of these values: [\'required\', \'preferred\']'));
   });
 
   it('should return error if verifiableCredential\'s matching property is not boolean', function () {
-    const pdSchema: PresentationDefinition = getFile('./test/dif_pe_examples/pd/pd-simple-schema-age-predicate.json');
-    const vpSimple = getFile('./test/dif_pe_examples/pd/vp-simple-age-predicate.json');
+    const pdSchema: PresentationDefinition = getFile('./test/dif_pe_examples/pd/pd-simple-schema-age-predicate.json').presentation_definition;
+    const vpSimple = getFile('./test/dif_pe_examples/vp/vp-simple-age-predicate.json');
     const eb: EvaluationBundler<InputDescriptor[], unknown> = new PredicateEB('root');
     const ee = new EvaluationEngine();
     const result = ee.evaluate([{bundler: eb, target: {d: pdSchema.input_descriptors, p: vpSimple}}])
