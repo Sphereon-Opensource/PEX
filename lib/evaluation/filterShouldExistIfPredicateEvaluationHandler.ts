681f7918d24cea796156e56b1eb193c960d222a3
ksadjad <sadjad.khoshkhou@gmail.com>
2021-07-22 10:41:20 +0200

    ESSIFI-104: updating result for input_descriptor instead of throwing exception

--- lib/evaluation/filterShouldExistIfPredicateEvaluationHandler.ts
+++ lib/evaluation/filterShouldExistIfPredicateEvaluationHandler.ts
@@ -1,22 +1,34 @@
 import { Field, InputDescriptor, PresentationDefinition } from '@sphereon/pe-models';
 
-import { Checked } from '../ConstraintUtils';
+import { Checked, Status } from '../ConstraintUtils';
 
 import { AbstractEvaluationHandler } from './abstractEvaluationHandler';
 
 export class FilterShouldExistIfPredicateEvaluationHandler extends AbstractEvaluationHandler {
-  // This incorrect format of input_descriptor is a deal-breaker for us, and therefore we throw exception for it
+
+  failed_checked: Checked = {
+    tag: 'root.input_descriptor',
+    status: Status.ERROR,
+    message: "if in the field we have predicate value, the filter value should be present as well.",
+  };
+
   public handle(pd: PresentationDefinition, p: unknown, result: Map<InputDescriptor, Map<unknown, Checked>>): void {
-    // HERE we process the predicate part of the PD
     for (let i = 0; i < pd.input_descriptors.length; i++) {
       const inputDescriptor: InputDescriptor = pd.input_descriptors[i];
-      for (let j = 0; j < inputDescriptor.constraints.fields.length; j++) {
-        const field: Field = inputDescriptor.constraints.fields[j];
-        if (field.predicate && !field.filter) {
-          throw new Error('if in the field we have predicate value, the filter value should be present as well.');
+      if (inputDescriptor.constraints && inputDescriptor.constraints.fields) {
+        for (let j = 0; j < inputDescriptor.constraints.fields.length; j++) {
+          const field: Field = inputDescriptor.constraints.fields[j];
+          if (field.predicate && !field.filter) {
+            this.updateAllResultsForInputDescriptor(p, result.get(inputDescriptor))
+          }
         }
       }
     }
-    super.handle(pd, p, result);
+  }
+
+  private updateAllResultsForInputDescriptor(verifiablePresentation: any, vcResult: Map<unknown, Checked>) {
+    verifiablePresentation.verifiableCredential.foreach(vc => {
+      vcResult.set(vc, this.failed_checked);
+    });
   }
 }
