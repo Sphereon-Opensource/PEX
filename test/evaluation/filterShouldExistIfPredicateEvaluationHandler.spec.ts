cf322480e9c8ec7082fb402364735de2aeb62d1b
ksadjad <sadjad.khoshkhou@gmail.com>
2021-07-23 09:31:05 +0200

    ESSIFI-104: added a success test for the FilterShouldExistIfPredicate evaluator

--- test/evaluation/filterShouldExistIfPredicateEvaluationHandler.spec.ts
+++ test/evaluation/filterShouldExistIfPredicateEvaluationHandler.spec.ts
@@ -21,4 +21,12 @@ describe('evaluate', () => {
     const result: HandlerCheckResult[] = evaluationHandler.handle(pdSchema, vpSimple);
     expect(result[0]).toEqual(new HandlerCheckResult('root.input_descriptors[0].constraints.fields[0]', '', 'FilterShouldExistIfPredicate', Status.ERROR, "if in the field we have predicate value, the filter value should be present as well."));
   });
+
+  it('should report info if the predicate and filter are both present', function () {
+    const pdSchema: PresentationDefinition = getFile('./test/dif_pe_examples/pd/pd-simple-schema-age-predicate.json').presentation_definition;
+    const vpSimple = getFile('./test/dif_pe_examples/vp/vp-simple-age-predicate.json');
+    const evaluationHandler: EvaluationHandler = new FilterShouldExistIfPredicateEvaluationHandler();
+    const result: HandlerCheckResult[] = evaluationHandler.handle(pdSchema, vpSimple);
+    expect(result[0]).toEqual(new HandlerCheckResult('root.input_descriptors[0].constraints.fields[0]', '', 'FilterShouldExistIfPredicate', Status.INFO, "predicate value and the filter value are both present."));
+  });
 });
