bb446254f2750fe5e3911dea88cfe5e5de1893ea
Maikel Maas <mmaas@sphereon.com>
2021-08-05 13:54:17 +0200

    ESSIFI-68 Step 3 - Mark the candidate input as a match for use in a Presentation Submission
       - Removed result duplication;

--- test/evaluation/evaluationClient.spec.ts
+++ test/evaluation/evaluationClient.spec.ts
@@ -2,8 +2,8 @@ import fs from 'fs';
 
 import { Optionality, PresentationDefinition } from '@sphereon/pe-models';
 
-import { EvaluationClient } from "../../lib/evaluation/evaluationClient";
 import { Status } from '../../lib';
+import { EvaluationClient } from "../../lib/evaluation/evaluationClient";
 
 function getFile(path: string) {
   return JSON.parse(fs.readFileSync(path, 'utf-8'));
@@ -137,7 +137,7 @@ describe('evaluate', () => {
     const errorResults = evaluationClient.results.filter(result => result.status === Status.ERROR);
     const infoResults = evaluationClient.results.filter(result => result.status === Status.INFO);
     expect(errorResults.length).toEqual(5);
-    expect(infoResults.length).toEqual(8);
+    expect(infoResults.length).toEqual(7);
   });
 
 
