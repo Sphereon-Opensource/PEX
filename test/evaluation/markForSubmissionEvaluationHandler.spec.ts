f93824d0aa9df076c5a59e5660315231c105ded2
Maikel Maas <mmaas@sphereon.com>
2021-08-05 14:16:08 +0200

    ESSIFI-68 Step 3 - Mark the candidate input as a match for use in a Presentation Submission
       - Added a test case for error result;

--- test/evaluation/markForSubmissionEvaluationHandler.spec.ts
+++ test/evaluation/markForSubmissionEvaluationHandler.spec.ts
@@ -32,6 +32,32 @@ const results: HandlerCheckResult[] = [
   }
 ]
 
+const results_with_error: HandlerCheckResult[] = [
+  {
+    input_descriptor_path: '$.input_descriptors[0]',
+    verifiable_credential_path: '$.verifiableCredential[0]',
+    evaluator: 'UriEvaluation',
+    status: 'info',
+    message: 'presentation_definition URI for the schema of the candidate input is equal to one of the input_descriptors object uri values.'
+  },
+  {
+    input_descriptor_path: '$.input_descriptors[0]',
+    verifiable_credential_path: '$.verifiableCredential[0]',
+    evaluator: 'FilterEvaluation',
+    status: 'info',
+    message: 'Input candidate valid for presentation submission',
+    payload: { result: { value: 'eu', path: ["$", "details", "citizenship"]}, valid: true }
+  },
+  {
+    input_descriptor_path: '$.input_descriptors[0]',
+    verifiable_credential_path: '$.verifiableCredential[0]',
+    evaluator: 'PredicateRelatedField',
+    status: 'error',
+    message: 'Input candidate invalid for presentation submission',
+    payload: { value: false, path: ["$", "age"] }
+  }
+]
+
 function getFile(path: string): unknown {
   return JSON.parse(fs.readFileSync(path, 'utf-8'));
 }
@@ -63,6 +89,30 @@ describe('markForSubmissionEvaluationHandler tests', () => {
         id: "banking_input_1",
         path: "$.verifiableCredential[0]",
       }]
+    }));
+  });
+
+  it(`Mark input candidates for presentation submission with errors`, () => {
+    const inputCandidates: unknown = getFile('./test/dif_pe_examples/vp/vp_general.json');
+    const presentationDefinition: PresentationDefinition = getFile('./test/resources/pd_input_descriptor_filter.json')['presentation_definition'];
+    presentationDefinition.input_descriptors = [presentationDefinition.input_descriptors[0]];
+    const evaluationClient: EvaluationClient = new EvaluationClient();
+    evaluationClient.results.push(...results_with_error)
+    const evaluationHandler = new MarkForSubmissionEvaluationHandler(evaluationClient);
+    evaluationHandler.handle(presentationDefinition, inputCandidates);
+    const length = evaluationHandler.getResults().length;
+    expect(evaluationHandler.getResults()[length - 1]).toEqual({
+      evaluator: "MarkForSubmissionEvaluation",
+      input_descriptor_path: "$.input_descriptors[0]",
+      message: "The input candidate is not eligible for submission",
+      payload: { "evaluator": "PredicateRelatedField", "path": [ "$", "age" ], "value": false },
+      status: "error",
+      verifiable_credential_path: "$.verifiableCredential[0]"
+    });
+    expect(evaluationHandler.verifiablePresentation.presentationSubmission).toEqual(
+      expect.objectContaining({
+      definition_id: "32f54163-7166-48f1-93d8-ff217bdb0653",
+      descriptor_map: []
     }));
   });
 
