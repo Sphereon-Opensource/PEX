4c2c5e63e207e8a7bca14ab55f6cb40e6f204069
Maikel Maas <mmaas@sphereon.com>
2021-08-06 16:55:54 +0200

    Revert "ESSIFI-109 Create a wrapper function for evaluate function"
    This reverts commit b106ff214f447e1267d962697ae4aed2378275b4.

--- lib/evaluation/evaluationClient.ts
+++ lib/evaluation/evaluationClient.ts
@@ -3,8 +3,6 @@ import { PresentationDefinition } from '@sphereon/pe-models';
 import { Checked, Status } from '../ConstraintUtils';
 
 import { EvaluationHandler } from './evaluationHandler';
-import { EvaluationResults } from './evaluationResults';
-//import { EvaluationResults } from './evaluationResults';
 import { HandlerCheckResult } from './handlerCheckResult';
 import { InputDescriptorFilterEvaluationHandler } from './inputDescriptorFilterEvaluationHandler';
 import { LimitDisclosureEvaluationHandler } from './limitDisclosureEvaluationHandler';
@@ -28,7 +26,7 @@ export class EvaluationClient {
   private _results: HandlerCheckResult[];
   private _verifiablePresentation: unknown;
 
-  public evaluate(pd: PresentationDefinition, vp: unknown): EvaluationResults {
+  public evaluate(pd: PresentationDefinition, vp: unknown): HandlerCheckResult[] {
     let currentHandler: EvaluationHandler = this.initEvaluationHandlers();
     currentHandler.handle(pd, vp);
     while (currentHandler.hasNext()) {
@@ -40,18 +38,7 @@ export class EvaluationClient {
         throw this.failed_catched;
       }
     }
-    return this.getEvalutionResults();
-  }
-
-  private getEvalutionResults(): EvaluationResults {
-    const result: any = {};
-    result.warnings = this.results.filter(result => result.status === Status.WARN).map(x => JSON.stringify(x));
-    result.errors = this.results.filter(result => result.status === Status.ERROR)
-    .map(x => { return { name: x.evaluator, message: `${x.message}: ${x.input_descriptor_path}: ${x.verifiable_credential_path}` } });
-    if (this._verifiablePresentation['presentationSubmission']["descriptor_map"].length) {
-      result.value = this._verifiablePresentation["presentationSubmission"];
-    }
-    return result;
+    return this._results;
   }
 
   get results(): HandlerCheckResult[] {
--- lib/evaluation/evaluationResults.ts
+++
@@ -1,7 +0,0 @@
-import { PresentationSubmission } from "@sphereon/pe-models";
-
-export interface EvaluationResults {
-    value?: PresentationSubmission;
-    warnings?: string[];
-    errors?: Error[];
-}
