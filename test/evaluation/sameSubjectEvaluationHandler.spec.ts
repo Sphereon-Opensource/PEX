f834c9c87d0fdd3660546b0dbad98354c59add34
ksadjad <sadjad.khoshkhou@gmail.com>
2021-08-17 11:58:46 +0200

    removed commented tests

--- test/evaluation/sameSubjectEvaluationHandler.spec.ts
+++ test/evaluation/sameSubjectEvaluationHandler.spec.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-import {PresentationDefinition} from '@sphereon/pe-models';
+import { PresentationDefinition } from '@sphereon/pe-models';
 
 import {
   EvaluationClient,
@@ -26,25 +26,4 @@ describe('sameSubjectEvaluationHandler', () => {
     evaluationHandler.handle(pd, vp as VP);
     expect(evaluationHandler.client.results).toEqual(results);
   });
-
-  // it('Should record as error when the fields requiring same subject do not belong to same subjects', () => {
-  //   const pdSchema: PresentationDefinition = getFile('./test/dif_pe_examples/pd/pd-simple-schema-age-predicate.json').presentation_definition;
-  //   const vpSimple = getFile('./test/dif_pe_examples/vp/vp-simple-age-predicate.json');
-  //   vpSimple.verifiableCredential[0].credentialSchema[0].id = "https://www.test.org/mock"
-  //   const evaluationHandler: EvaluationHandler = new SameSubjectEvaluationHandler();
-  //   const results: HandlerCheckResult[] = [];
-  //   evaluationHandler.handle(pdSchema, vpSimple, results);
-  //   expect(results[0]).toEqual(new HandlerCheckResult('$.input_descriptors[0]', "$.verifiableCredential[0]", "UriEvaluation", Status.ERROR, "presentation_definition URI for the schema of the candidate input MUST be equal to one of the input_descriptors object uri values exactly."));
-  // });
-  //
-  // it('Should record as warning when the fields preferring same subject do not belong to same subjects', () => {
-  //   const pdSchema: PresentationDefinition = getFile('./test/dif_pe_examples/pd/pd-simple-schema-age-predicate.json').presentation_definition;
-  //   const vpSimple = getFile('./test/dif_pe_examples/vp/vp-simple-age-predicate.json');
-  //   vpSimple.verifiableCredential[0].credentialSchema[0].id = "https://www.test.org/mock"
-  //   const evaluationHandler: EvaluationHandler = new SameSubjectEvaluationHandler();
-  //   const results: HandlerCheckResult[] = [];
-  //   evaluationHandler.handle(pdSchema, vpSimple, results);
-  //   expect(results[0]).toEqual(new HandlerCheckResult('$.input_descriptors[0]', "$.verifiableCredential[0]", "UriEvaluation", Status.ERROR, "presentation_definition URI for the schema of the candidate input MUST be equal to one of the input_descriptors object uri values exactly."));
-  // });
-
 });
