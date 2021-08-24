b54c6fbc6cdecbf4fbd760fa3a25af2391a33b6d
Maikel Maas <mmaas@sphereon.com>
2021-08-24 17:18:11 +0200

    ESSIFI-71 Step 6 - Ensure data submissions are Fulfilled by Subject of the provided attributes
       - Fields are now correctly matched against the credential subject;

--- lib/evaluation/subjectIsHolderEvaluationHandler.ts
+++ lib/evaluation/subjectIsHolderEvaluationHandler.ts
@@ -1,9 +1,11 @@
-import { HolderSubject, InputDescriptor, PresentationDefinition } from '@sphereon/pe-models';
+import { Descriptor, InputDescriptor, Optionality, PresentationDefinition } from '@sphereon/pe-models';
+import jp from 'jsonpath';
 
 import { Status } from '../ConstraintUtils';
-import { VerifiableCredential, VerifiablePresentation } from '../verifiablePresentation';
+import { VerifiablePresentation } from '../verifiablePresentation';
 
 import { AbstractEvaluationHandler } from './abstractEvaluationHandler';
+import { EvaluationClient } from './evaluationClient';
 import { HandlerCheckResult } from './handlerCheckResult';
 
 export class SubjectIsHolderEvaluationHandler extends AbstractEvaluationHandler {
@@ -11,73 +13,232 @@ export class SubjectIsHolderEvaluationHandler extends AbstractEvaluationHandler 
     return 'IsHolderEvaluation';
   }
 
-  public handle(d: PresentationDefinition, p: VerifiablePresentation): void {
-    this.iterateOverInputCandidates(d, p);
+  private pDefinition: PresentationDefinition;
+  private vPresentation: VerifiablePresentation;
+
+  private readonly fieldIdzInputDescriptorsIsHolderRequired: Map<Set<string>, Set<string>>;
+  private readonly fieldIdzInputDescriptorsIsHolderPreferred: Map<Set<string>, Set<string>>;
+  private readonly allDescribedCredentialsPaths: Map<string, string>;
+
+  private credentialsSubjects: Map<string, any>;
+
+  private messages: Map<Status, string>;
+
+  constructor(client: EvaluationClient) {
+    super(client);
+
+    this.fieldIdzInputDescriptorsIsHolderRequired = new Map<Set<string>, Set<string>>();
+    this.fieldIdzInputDescriptorsIsHolderPreferred = new Map<Set<string>, Set<string>>();
+    this.allDescribedCredentialsPaths = new Map<string, string>();
+
+    this.credentialsSubjects = new Map<string, string>();
+
+    this.messages = new Map<Status, string>();
+    this.messages.set(Status.INFO, 'The field ids requiring the subject to be the holder');
+    this.messages.set(Status.WARN, 'The field ids preferring the subject to be the holder');
+    this.messages.set(Status.ERROR, 'The field ids requiring the subject to be the holder');
   }
 
-  private iterateOverInputCandidates(pd: PresentationDefinition, vp: VerifiablePresentation): void {
-    for (const vc of vp.getVerifiableCredentials().entries()) {
-      this.iterateOverInputDescriptors(pd, vc);
+  public handle(pd: PresentationDefinition): void {
+    this.pDefinition = pd;
+    this.vPresentation = this.client.verifiablePresentation;
+
+    this.findIsHolderFieldIdsToInputDescriptorsSets();
+    this.findAllDescribedCredentialsPaths();
+    this.findAllCredentialSubjects();
+
+    this.confirmAllFieldSetHasSameHolder(this.fieldIdzInputDescriptorsIsHolderRequired, Status.ERROR);
+    this.confirmAllFieldSetHasSameHolder(this.fieldIdzInputDescriptorsIsHolderPreferred, Status.WARN);
+  }
+
+  private findIsHolderFieldIdsToInputDescriptorsSets() {
+    this.pDefinition.input_descriptors.forEach(this.mapFieldIdsToInputDescriptors());
+  }
+
+  private mapFieldIdsToInputDescriptors() {
+    return (inputDescriptor) => {
+      inputDescriptor.constraints.is_holder?.forEach(this.mapIsHolderToInputDescriptors(inputDescriptor));
+    };
+  }
+
+  private mapIsHolderToInputDescriptors(inDesc: InputDescriptor) {
+    return (sameSubjectGroup) => {
+      let fieldIdzInputDescriptors;
+
+      if (sameSubjectGroup.directive === Optionality.Required) {
+        fieldIdzInputDescriptors = this.fieldIdzInputDescriptorsIsHolderRequired;
+      } else if (sameSubjectGroup.directive === Optionality.Preferred) {
+        fieldIdzInputDescriptors = this.fieldIdzInputDescriptorsIsHolderPreferred;
+      }
+
+      this.upsertFieldIdToInputDescriptorMapping(fieldIdzInputDescriptors, sameSubjectGroup.field_id, inDesc.id);
+    };
+  }
+
+  /**
+   * Update or insert the value in the map.
+   *
+   * @param fieldIdzInputDescriptors the map among which the value will be upserted.
+   * @param searchableFieldIds the fields which are being added now
+   * @param inDescId the input descriptor ids which is being mapped by the field ids
+   *
+   * @private
+   */
+  private upsertFieldIdToInputDescriptorMapping(
+    fieldIdzInputDescriptors: Map<Set<string>, Set<string>>,
+    searchableFieldIds: Array<string>,
+    inDescId: string
+  ) {
+    const inputDescriptorIds: Array<string> = this.getAllInputDescriptorsWithAnyOfTheseFields(searchableFieldIds);
+    inputDescriptorIds.push(inDescId);
+
+    if (this.getValue(fieldIdzInputDescriptors, searchableFieldIds) == null) {
+      this.addEntry(fieldIdzInputDescriptors, searchableFieldIds, inputDescriptorIds);
+    } else {
+      this.updateEntry(fieldIdzInputDescriptors, searchableFieldIds, inputDescriptorIds);
     }
   }
 
-  private iterateOverInputDescriptors(pd: PresentationDefinition, vc: [number, VerifiableCredential]): void {
-    const inputDescriptors: InputDescriptor[] = pd.input_descriptors;
-    const credentialSubject = this.retrieveVcSubjectId(vc[1]);
-    inputDescriptors.forEach((inDesc, index) => {
-      if (inDesc.constraints && inDesc.constraints.is_holder) {
-        const did = this.client.did;
-        inDesc.constraints.is_holder.forEach((ih) => {
-          const resultObject = this.checkIsHolder(index, vc, credentialSubject, did);
-          this.checkDirectives(ih, resultObject);
-        });
+  getAllInputDescriptorsWithAnyOfTheseFields(searchableFieldIds: Array<string>): Array<string> {
+    return this.pDefinition.input_descriptors
+      .filter(this.inputDescriptorsWithSameFields(searchableFieldIds))
+      .map((filteredInDesces) => filteredInDesces.id);
+  }
+
+  private inputDescriptorsWithSameFields(searchableFieldIds: Array<string>) {
+    return (inDesc) =>
+      inDesc.constraints.fields.filter(this.fieldExistsInInputDescriptor(searchableFieldIds)).length > 0;
+  }
+
+  private fieldExistsInInputDescriptor(searchableFieldIds: Array<string>) {
+    return (value) => searchableFieldIds.includes(value.id);
+  }
+
+  getValue(
+    fieldIdzInputDescriptors: Map<Set<string>, Set<string>>,
+    searchableFieldIds: Array<string>
+  ): { mappedFieldIds; mappedInputDescriptorIds } {
+    let entry: { mappedFieldIds: Set<string>; mappedInputDescriptorIds: Set<string> } = null;
+
+    for (const [mappedFieldIds, mappedInputDescriptorIds] of fieldIdzInputDescriptors.entries()) {
+      if (Array.from(mappedFieldIds.values()).filter((value) => searchableFieldIds.includes(value)).length > 0) {
+        entry = { mappedFieldIds, mappedInputDescriptorIds };
+      }
+    }
+
+    return entry;
+  }
+
+  private addEntry(
+    fieldIdzInputDescriptors: Map<Set<string>, Set<string>>,
+    fieldIds: Array<string>,
+    inputDescriptorIds: Array<string>
+  ) {
+    const addableFieldIds = new Set<string>(fieldIds);
+    const addableInputDescriptors = new Set<string>(inputDescriptorIds);
+
+    fieldIdzInputDescriptors.set(addableFieldIds, addableInputDescriptors);
+  }
+
+  private updateEntry(
+    fieldIdzInputDescriptors: Map<Set<string>, Set<string>>,
+    searchableFieldIds: Array<string>,
+    inputDescriptorIds: Array<string>
+  ) {
+    const entry = this.getValue(fieldIdzInputDescriptors, searchableFieldIds);
+
+    searchableFieldIds.forEach((searchableFieldId) => entry.mappedFieldIds.add(searchableFieldId));
+
+    inputDescriptorIds.forEach((inputDescriptorId) => entry.mappedInputDescriptorIds.add(inputDescriptorId));
+  }
+
+  private findAllDescribedCredentialsPaths() {
+    if (this.vPresentation.getPresentationSubmission()) {
+      this.vPresentation.getPresentationSubmission().descriptor_map.forEach(this.descriptorToPathMapper());
+    }
+  }
+
+  private descriptorToPathMapper() {
+    return (descriptor) => this.findDescribedCredentialPaths(descriptor);
+  }
+
+  private findDescribedCredentialPaths(descriptor: Descriptor) {
+    this.allDescribedCredentialsPaths.set(descriptor.id, descriptor.path);
+
+    if (descriptor.path_nested) {
+      this.findDescribedCredentialPaths(descriptor.path_nested);
+    }
+  }
+
+  private findAllCredentialSubjects() {
+    this.allDescribedCredentialsPaths.forEach(this.mapCredentialPathToCredentialSubject());
+  }
+
+  private mapCredentialPathToCredentialSubject() {
+    return (path, inDescId) => {
+      const subjectNode = jp.nodes(this.vPresentation.getRoot(), path.concat('..credentialSubject'));
+      if (subjectNode.length) {
+        this.credentialsSubjects.set(inDescId, subjectNode[0]);
+      }
+    };
+  }
+
+  private confirmAllFieldSetHasSameHolder(
+    fieldIdzInputDescriptorsGroups: Map<Set<string>, Set<string>>,
+    status: Status
+  ) {
+    fieldIdzInputDescriptorsGroups.forEach(this.confirmFieldSetHasSameHolder(status));
+  }
+
+  private confirmFieldSetHasSameHolder(status: 'info' | 'warn' | 'error') {
+    return (inputDescriptorIds: Set<string>, fieldIdSet: Set<string>) => {
+      const credentialSubjectsSet: Set<unknown> = new Set<unknown>();
+      inputDescriptorIds.forEach((inDescId) =>
+        credentialSubjectsSet.add({ ...this.credentialsSubjects.get(inDescId) })
+      );
+      this.addResult(credentialSubjectsSet, fieldIdSet, status);
+    };
+  }
+
+  private addResult(credentialSubjectsSet: Set<unknown>, fieldIdSet: Set<string>, status: Status) {
+    let myStatus: Status = status === Status.ERROR ? Status.INFO : status;
+    credentialSubjectsSet.forEach((cs) => {
+      if (cs['value']['id'] !== this.client.did) {
+        myStatus = Status.ERROR;
       }
     });
-  }
-
-  public retrieveVcSubjectId(vc: VerifiableCredential): string {
-    let did: string;
-    if (vc && vc.credentialSubject) {
-      did = vc.credentialSubject.id;
-    } else if (vc['vc'] && vc['vc'].credentialSubject) {
-      did = vc['vc'].credentialSubject.id;
+    const intersection = Array.from(fieldIdSet).filter(
+      (value) => !this.getCredentialFields(credentialSubjectsSet).includes(value)
+    );
+    if (intersection.length > 0) {
+      myStatus = Status.ERROR;
     }
-    return did;
+    this.getResults().push(this.getResult(fieldIdSet, credentialSubjectsSet, myStatus));
   }
 
-  private checkDirectives(hs: HolderSubject, resultObject: HandlerCheckResult): void {
-    if (hs.directive === 'preferred') {
-      delete resultObject.payload['holder'];
-      delete resultObject.payload['subject'];
-    } else if (hs.directive === 'required') {
-      delete resultObject.payload['is_holder'];
-    }
-    this.client.results.push(resultObject);
-  }
-
-  private checkIsHolder(
-    index: number,
-    vc: [number, VerifiableCredential],
-    result: any,
-    did: string
+  private getResult(
+    fieldIdSet: Set<string>,
+    credentialSubjectsSet: Set<unknown>,
+    myStatus: Status
   ): HandlerCheckResult {
-    const resultObject = this.createResultObject(index, vc[0]);
-    resultObject.payload = { holder: did, subject: result, is_holder: true };
-    if (result !== did) {
-      resultObject.status = Status.ERROR;
-      resultObject.message = 'The entity submitting the response is not the holder of the claim';
-      resultObject.payload.is_holder = false;
-    }
-    return resultObject;
+    const paths: Array<string> = Array.from(credentialSubjectsSet).map((el) => jp.stringify(el['path'].slice(0, 3)));
+    const inputDescriptorPath = '[' + Array.from(fieldIdSet).join(',') + ']';
+    const verifiableCredentialPath = '[' + paths.join(',') + ']';
+    const credentialFields: Array<string> = this.getCredentialFields(credentialSubjectsSet);
+    return {
+      input_descriptor_path: inputDescriptorPath,
+      verifiable_credential_path: verifiableCredentialPath,
+      evaluator: this.getName(),
+      status: myStatus,
+      payload: { fieldIdSet: inputDescriptorPath, credentialSubjectsSet: credentialFields },
+      message: this.messages.get(myStatus),
+    };
   }
 
-  private createResultObject(idIndex: number, vcIndex: number): HandlerCheckResult {
-    return {
-      input_descriptor_path: `$.input_descriptors[${idIndex}]`,
-      verifiable_credential_path: `$.verifiableCredential[${vcIndex}]`,
-      evaluator: this.getName(),
-      status: Status.INFO,
-      message: 'The entity submitting the response is the holder of the claim',
-    };
+  private getCredentialFields(credentialSubjectsSet: Set<unknown>): string[] {
+    return Array.from(credentialSubjectsSet)
+      .map((el) => Object.keys(el['value']))
+      .reduce((acc, val) => acc.concat(val))
+      .filter((x) => x !== 'id');
   }
 }
--- test/evaluation/subjectIsHolderEvaluationHandler.spec.ts
+++ test/evaluation/subjectIsHolderEvaluationHandler.spec.ts
@@ -2,9 +2,8 @@ import fs from 'fs';
 
 import { PresentationDefinition } from '@sphereon/pe-models';
 
-import { VP } from '../../lib';
+import { Presentation, SubjectIsHolderEvaluationHandler, VP } from '../../lib';
 import { EvaluationClient } from '../../lib/evaluation/evaluationClient';
-import { Presentation } from '../../lib/verifiablePresentation/models';
 
 function getFile(path: string): unknown {
   return JSON.parse(fs.readFileSync(path, 'utf-8'));
@@ -15,70 +14,14 @@ const did = 'did:example:ebfeb1f712ebc6f1c276e12ec21';
 describe('SubjectIsHolderEvaluationHandler tests', () => {
 
   it(`input descriptor's constraints.is_holder is present`, () => {
-    const presentation: Presentation = getFile('./test/dif_pe_examples/vp/vp_subject_is_holder.json') as Presentation;
-    const presentationDefinition: PresentationDefinition = getFile('./test/resources/pd_input_descriptor_is_holder.json')['presentation_definition'];
-    presentationDefinition.input_descriptors = [presentationDefinition.input_descriptors[0]];
-    
+    const presentationDefinition: PresentationDefinition = getFile('./test/resources/pd_require_is_holder.json')['presentation_definition'];
+    const results = getFile('./test/resources/isHolderEvaluationResults.json');
     const evaluationClient: EvaluationClient = new EvaluationClient();
-    evaluationClient.evaluate(presentationDefinition, new VP(presentation), did);
-    expect(evaluationClient.results.filter(r => r.evaluator === "IsHolderEvaluation")).toEqual([
-      {
-        "evaluator": "IsHolderEvaluation",
-        "input_descriptor_path": "$.input_descriptors[0]",
-        "message": "The entity submitting the response is the holder of the claim",
-        "payload": { "is_holder": true },
-        "status": "info",
-        "verifiable_credential_path": "$.verifiableCredential[0]"
-      },
-      {
-        "evaluator": "IsHolderEvaluation",
-        "input_descriptor_path": "$.input_descriptors[0]",
-        "message": "The entity submitting the response is the holder of the claim",
-        "payload": {
-          "holder": "did:example:ebfeb1f712ebc6f1c276e12ec21",
-          "subject": "did:example:ebfeb1f712ebc6f1c276e12ec21",
-        },
-        "status": "info",
-        "verifiable_credential_path": "$.verifiableCredential[0]"
-      },
-      {
-        "evaluator": "IsHolderEvaluation",
-        "input_descriptor_path": "$.input_descriptors[0]",
-        "message": "The entity submitting the response is the holder of the claim",
-        "payload": { "is_holder": true },
-        "status": "info", 
-        "verifiable_credential_path": "$.verifiableCredential[1]"
-      },
-      {
-        "evaluator": "IsHolderEvaluation",
-        "input_descriptor_path": "$.input_descriptors[0]",
-        "message": "The entity submitting the response is the holder of the claim",
-        "payload": {
-          "holder": "did:example:ebfeb1f712ebc6f1c276e12ec21",
-          "subject": "did:example:ebfeb1f712ebc6f1c276e12ec21",
-        },
-        "status": "info",
-        "verifiable_credential_path": "$.verifiableCredential[1]"
-      },
-      {
-        "evaluator": "IsHolderEvaluation",
-        "input_descriptor_path": "$.input_descriptors[0]",
-        "message": "The entity submitting the response is not the holder of the claim",
-        "payload": { "is_holder": false },
-        "status": "error",
-        "verifiable_credential_path": "$.verifiableCredential[2]"
-      },
-      {
-        "evaluator": "IsHolderEvaluation",
-        "input_descriptor_path": "$.input_descriptors[0]",
-        "message": "The entity submitting the response is not the holder of the claim",
-        "payload": {
-          "holder": "did:example:ebfeb1f712ebc6f1c276e12ec21",
-          "subject": "did:example:ebfeb1f712ebc6f1c276e12ec22",
-        },
-        "status": "error",
-        "verifiable_credential_path": "$.verifiableCredential[2]"
-      }
-    ]);
+    evaluationClient.did = did;
+    const evaluationHandler: SubjectIsHolderEvaluationHandler = new SubjectIsHolderEvaluationHandler(evaluationClient);
+    const presentation: Presentation = getFile('./test/dif_pe_examples/vp/vp_subject_is_holder.json') as Presentation;
+    evaluationClient.verifiablePresentation = new VP(presentation);
+    evaluationHandler.handle(presentationDefinition);
+    expect(evaluationHandler.client.results).toEqual(results);
   });
 });
