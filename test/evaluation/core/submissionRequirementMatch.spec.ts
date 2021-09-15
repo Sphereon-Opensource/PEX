f500ddf9e36df4020b4236be816af22aa9962905
ksadjad <sadjad.khoshkhou@gmail.com>
2021-09-15 12:06:24 +0200

    ESSIFI-112: added a constructor test for submissionRequirementMatch creating object

---
+++ test/evaluation/core/submissionRequirementMatch.spec.ts
@@ -0,0 +1,21 @@
+import { Rules } from '@sphereon/pe-models';
+
+import { SubmissionRequirementMatch } from '../../../lib';
+
+
+describe('submissionRequirementMatch', () => {
+
+  it('should return ok constructor works correctly', function () {
+    const submissionRequirementMatch: SubmissionRequirementMatch = new SubmissionRequirementMatch(
+        "test srm",
+        Rules.All,
+        1,
+        ["$.verifiableCredential[1]"],
+        ["A"],
+        null
+    );
+    expect(submissionRequirementMatch.count).toBe(1);
+    expect(submissionRequirementMatch.rule).toBe(Rules.All);
+    expect(submissionRequirementMatch.matches[0]).toBe("$.verifiableCredential[1]");
+  });
+});
