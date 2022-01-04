7fd61846b320bb124f180dbc9efc02eb6b92076a
Maikel Maas <mmaas@sphereon.com>
2022-01-04 17:16:48 +0100

    ESSIFI-163 enhancements in processing jwt vc
       - Created properties validation;
       - Set the additional JWT properties;

--- lib/types/SSITypesBuilder.ts
+++ lib/types/SSITypesBuilder.ts
@@ -7,7 +7,7 @@ import {
   InternalVerifiableCredentialJsonLD,
   InternalVerifiableCredentialJwt,
 } from './Internal.types';
-import { IVerifiableCredential } from './SSI.types';
+import { IHasProof, IJwtCredential, IVerifiableCredential } from './SSI.types';
 
 export class SSITypesBuilder {
   public static createInternalPresentationDefinitionV1FromModelEntity(p: PdV1): InternalPresentationDefinitionV1 {
@@ -46,12 +46,47 @@ export class SSITypesBuilder {
   private static mapExternalVerifiableCredentialToInternal(externalCredential: IVerifiableCredential) {
     if (externalCredential.vc && externalCredential.iss) {
       const vc: InternalVerifiableCredential = new InternalVerifiableCredentialJwt();
-      return Object.assign(vc, externalCredential);
+      const result = Object.assign(vc, externalCredential);
+      return SSITypesBuilder.setJWTAdditionalFields(result);
     } else if (externalCredential.credentialSubject && externalCredential.id) {
       const vc: InternalVerifiableCredential = new InternalVerifiableCredentialJsonLD();
       return Object.assign(vc, externalCredential);
     }
     throw 'VerifiableCredential structure is incorrect.';
+  }
+
+  private static setJWTAdditionalFields(
+    result: InternalVerifiableCredentialJwt & IJwtCredential & IHasProof
+  ): InternalVerifiableCredentialJwt & IJwtCredential & IHasProof {
+    if (result.exp) {
+      if (result.expirationDate && result.expirationDate !== result.exp) {
+        throw new Error('Inconsistent expiration dates');
+      }
+      result.expirationDate = new Date(result.exp as string | number | Date).toISOString();
+    }
+    if (result.issuer !== result.iss) {
+      throw new Error('Inconsistent issuers');
+    }
+    result.issuer = result.iss;
+    if (result.nbf) {
+      if (result.issuanceDate && result.issuanceDate !== result.nbf) {
+        throw new Error('Inconsistent issuance dates');
+      }
+      result.issuanceDate = new Date(result.nbf as string | number | Date).toISOString();
+    }
+    if (result.sub) {
+      if (result.vc.credentialSubject.id && result.vc.credentialSubject.id !== result.sub) {
+        throw new Error('Inconsistent credential subject ids');
+      }
+      result.vc.credentialSubject.id = result.sub;
+    }
+    if (result.jti) {
+      if (result.id && result.id !== result.jti) {
+        throw new Error('Inconsistent credential ids');
+      }
+      result.id = result.jti;
+    }
+    return result;
   }
 
   static mapInternalVerifiableCredentialsToExternal(
