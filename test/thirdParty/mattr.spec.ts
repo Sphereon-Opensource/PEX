3c20ee71a9ada1dfce3e1b8b2c48f7ca6f3ae1bd
Timo Glastra <timo@animo.id>
2023-09-29 23:09:07 +0200

    test: update test with actual credential
    Signed-off-by: Timo Glastra <timo@animo.id>

--- test/thirdParty/mattr.spec.ts
+++ test/thirdParty/mattr.spec.ts
@@ -12,6 +12,7 @@ describe('evaluate mattr tests', () => {
   it('should pass with OpenBadgeCredential', () => {
     const pex: PEX = new PEX();
     const result = pex.evaluateCredentials(pd, vcs);
+    console.log(JSON.stringify(result, null, 2));
     expect(result.areRequiredCredentialsPresent).toEqual(Status.INFO);
   });
 
@@ -47,61 +48,7 @@ describe('evaluate mattr tests', () => {
   });
 
   const vcs: W3CVerifiableCredential[] = [
-    {
-      type: ['VerifiableCredential', 'VerifiableCredentialExtension', 'OpenBadgeCredential'],
-      issuer: {
-        id: 'did:web:launchpad.vii.electron.mattrlabs.io',
-        name: 'Jobs for the Future (JFF)',
-        iconUrl: 'https://w3c-ccg.github.io/vc-ed/plugfest-1-2022/images/JFF_LogoLockup.png',
-        image: 'https://w3c-ccg.github.io/vc-ed/plugfest-1-2022/images/JFF_LogoLockup.png',
-      },
-      name: 'JFF x vc-edu PlugFest 2',
-      description: "MATTR's submission for JFF Plugfest 2",
-      credentialBranding: {
-        backgroundColor: '#464c49',
-      },
-      issuanceDate: '2023-01-25T16:58:06.292Z',
-      credentialSubject: {
-        id: 'did:key:z6MkpGR4gs4Rc3Zph4vj8wRnjnAxgAPSxcR8MAVKutWspQzc',
-        type: ['AchievementSubject'],
-        achievement: {
-          id: 'urn:uuid:bd6d9316-f7ae-4073-a1e5-2f7f5bd22922',
-          name: 'JFF x vc-edu PlugFest 2 Interoperability',
-          type: ['Achievement'],
-          image: {
-            id: 'https://w3c-ccg.github.io/vc-ed/plugfest-2-2022/images/JFF-VC-EDU-PLUGFEST2-badge-image.png',
-            type: 'Image',
-          },
-          criteria: {
-            type: 'Criteria',
-            narrative:
-              'Solutions providers earned this badge by demonstrating interoperability between multiple providers based on the OBv3 candidate final standard, with some additional required fields. Credential issuers earning this badge successfully issued a credential into at least two wallets.  Wallet implementers earning this badge successfully displayed credentials issued by at least two different credential issuers.',
-          },
-          description:
-            'This credential solution supports the use of OBv3 and w3c Verifiable Credentials and is interoperable with at least two other solutions.  This was demonstrated successfully during JFF x vc-edu PlugFest 2.',
-        },
-      },
-      '@context': [
-        'https://www.w3.org/2018/credentials/v1',
-        {
-          '@vocab': 'https://w3id.org/security/undefinedTerm#',
-        },
-        'https://mattr.global/contexts/vc-extensions/v1',
-        'https://purl.imsglobal.org/spec/ob/v3p0/context.json',
-        'https://w3c-ccg.github.io/vc-status-rl-2020/contexts/vc-revocation-list-2020/v1.jsonld',
-      ],
-      credentialStatus: {
-        id: 'https://launchpad.vii.electron.mattrlabs.io/core/v1/revocation-lists/b4aa46a0-5539-4a6b-aa03-8f6791c22ce3#49',
-        type: 'RevocationList2020Status',
-      },
-      proof: {
-        type: 'Ed25519Signature2018',
-        created: '2023-01-25T16:58:07Z',
-        jws: 'eyJhbGciOiJFZERTQSIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..PrpRKt60yXOzMNiQY5bELX40F6Svwm-FyQ-Jv02VJDfTTH8GPPByjtOb_n3YfWidQVgySfGQ_H7VmCGjvsU6Aw',
-        proofPurpose: 'assertionMethod',
-        verificationMethod: 'did:web:launchpad.vii.electron.mattrlabs.io#6BhFMCGTJg',
-      },
-    },
+    'eyJhbGciOiJFZERTQSIsImtpZCI6ImRpZDp3ZWI6bGF1bmNocGFkLnZpaS5lbGVjdHJvbi5tYXR0cmxhYnMuaW8jNkJoRk1DR1RKZyJ9.eyJpc3MiOiJkaWQ6d2ViOmxhdW5jaHBhZC52aWkuZWxlY3Ryb24ubWF0dHJsYWJzLmlvIiwic3ViIjoiZGlkOmtleTp6Nk1raHpBUWpvVW1KQ21WVVFxMkJYQVVkWkJ1a3AxQXpYNGc5U0VOVUROWG9FRzEiLCJuYmYiOjE2OTU3MTk5MjksImV4cCI6MTcyNzM0MjMyOSwidmMiOnsiQGNvbnRleHQiOlsiaHR0cHM6Ly93d3cudzMub3JnLzIwMTgvY3JlZGVudGlhbHMvdjEiLCJodHRwczovL21hdHRyLmdsb2JhbC9jb250ZXh0cy92Yy1leHRlbnNpb25zL3YyIiwiaHR0cHM6Ly9wdXJsLmltc2dsb2JhbC5vcmcvc3BlYy9vYi92M3AwL2NvbnRleHQtMy4wLjIuanNvbiIsImh0dHBzOi8vcHVybC5pbXNnbG9iYWwub3JnL3NwZWMvb2IvdjNwMC9leHRlbnNpb25zLmpzb24iLCJodHRwczovL3czaWQub3JnL3ZjLXJldm9jYXRpb24tbGlzdC0yMDIwL3YxIl0sInR5cGUiOlsiVmVyaWZpYWJsZUNyZWRlbnRpYWwiLCJPcGVuQmFkZ2VDcmVkZW50aWFsIl0sImNyZWRlbnRpYWxTdWJqZWN0Ijp7ImlkIjoiZGlkOmtleTp6Nk1raHpBUWpvVW1KQ21WVVFxMkJYQVVkWkJ1a3AxQXpYNGc5U0VOVUROWG9FRzEiLCJ0eXBlIjpbIkFjaGlldmVtZW50U3ViamVjdCJdLCJhY2hpZXZlbWVudCI6eyJpZCI6Imh0dHBzOi8vZXhhbXBsZS5jb20vYWNoaWV2ZW1lbnRzLzIxc3QtY2VudHVyeS1za2lsbHMvdGVhbXdvcmsiLCJuYW1lIjoiVGVhbXdvcmsiLCJ0eXBlIjpbIkFjaGlldmVtZW50Il0sImltYWdlIjp7ImlkIjoiaHR0cHM6Ly93M2MtY2NnLmdpdGh1Yi5pby92Yy1lZC9wbHVnZmVzdC0zLTIwMjMvaW1hZ2VzL0pGRi1WQy1FRFUtUExVR0ZFU1QzLWJhZGdlLWltYWdlLnBuZyIsInR5cGUiOiJJbWFnZSJ9LCJjcml0ZXJpYSI6eyJuYXJyYXRpdmUiOiJUZWFtIG1lbWJlcnMgYXJlIG5vbWluYXRlZCBmb3IgdGhpcyBiYWRnZSBieSB0aGVpciBwZWVycyBhbmQgcmVjb2duaXplZCB1cG9uIHJldmlldyBieSBFeGFtcGxlIENvcnAgbWFuYWdlbWVudC4ifSwiZGVzY3JpcHRpb24iOiJUaGlzIGJhZGdlIHJlY29nbml6ZXMgdGhlIGRldmVsb3BtZW50IG9mIHRoZSBjYXBhY2l0eSB0byBjb2xsYWJvcmF0ZSB3aXRoaW4gYSBncm91cCBlbnZpcm9ubWVudC4ifX0sImlzc3VlciI6eyJpZCI6ImRpZDp3ZWI6bGF1bmNocGFkLnZpaS5lbGVjdHJvbi5tYXR0cmxhYnMuaW8iLCJuYW1lIjoiRXhhbXBsZSBVbml2ZXJzaXR5IiwiaWNvblVybCI6Imh0dHBzOi8vdzNjLWNjZy5naXRodWIuaW8vdmMtZWQvcGx1Z2Zlc3QtMS0yMDIyL2ltYWdlcy9KRkZfTG9nb0xvY2t1cC5wbmciLCJpbWFnZSI6Imh0dHBzOi8vdzNjLWNjZy5naXRodWIuaW8vdmMtZWQvcGx1Z2Zlc3QtMS0yMDIyL2ltYWdlcy9KRkZfTG9nb0xvY2t1cC5wbmcifX19.098G6WqIsCT2Sc4X3ioa8g_g382bknK-13_vhYHKa4w43e10RPhlg8-0Ir5roYnaYvoXIEW7pUAB-d5KJiO2AA',
   ];
 
   const pd = {
@@ -109,11 +56,13 @@ describe('evaluate mattr tests', () => {
     input_descriptors: [
       {
         id: 'OpenBadgeCredential',
+        // FIXME: need to update pex models
+        // @ts-ignore
         format: { jwt_vc_json: { alg: ['EdDSA'] }, jwt_vc: { alg: ['EdDSA'] } },
         constraints: {
           fields: [
             {
-              path: ['$.type'],
+              path: ['$.vc.type'],
               filter: {
                 type: 'array',
                 items: { type: 'string' },
