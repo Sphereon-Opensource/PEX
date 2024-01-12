4d75388f5ff9aa9d81e87deddf3870db3b86357c
José San Juan <jose@gataca.io>
2024-01-12 12:54:50 +0100

    Fix evaluation rule ALL when there are no marked input descriptors

--- lib/evaluation/evaluationClientWrapper.ts
+++ lib/evaluation/evaluationClientWrapper.ts
@@ -4,7 +4,7 @@ import { IVerifiableCredential, OriginalVerifiableCredential, WrappedVerifiableC
 
 import { Checked, Status } from '../ConstraintUtils';
 import { PresentationSubmissionLocation } from '../signing';
-import { IInternalPresentationDefinition, InternalPresentationDefinitionV2, IPresentationDefinition } from '../types';
+import { IInternalPresentationDefinition, IPresentationDefinition, InternalPresentationDefinitionV2 } from '../types';
 import { JsonPathUtils, ObjectUtils } from '../utils';
 
 import { EvaluationResults, HandlerCheckResult, SelectResults, SubmissionRequirementMatch } from './core';
@@ -478,7 +478,7 @@ export class EvaluationClientWrapper {
       if (sr.from) {
         if (sr.rule === Rules.All) {
           const [count, matched] = this.countMatchingInputDescriptors(sr, marked);
-          if (count !== groupCount.get(sr.from)) {
+          if (count !== (groupCount.get(sr.from) || 0)) {
             throw Error(`Not all input descriptors are members of group ${sr.from}`);
           }
           total++;
