08ba4fd26e7ce543762093451e70195a045f8ea1
Timo Glastra <timo@animo.id>
2024-04-25 11:01:11 +0200

    fix: const boolean value
    Signed-off-by: Timo Glastra <timo@animo.id>

--- resources/schema-generator-interfaces/filterV1.ts
+++ resources/schema-generator-interfaces/filterV1.ts
@@ -1,7 +1,8 @@
 import { OneOfNumberString } from './oneOfNumberString';
+import { OneOfNumberStringBoolean } from './oneOfNumberStringBoolean';
 
 export interface FilterV1Base {
-    const?: OneOfNumberString;
+    const?: OneOfNumberStringBoolean;
     enum?: Array<OneOfNumberString>;
     exclusiveMinimum?: OneOfNumberString;
     exclusiveMaximum?: OneOfNumberString;
